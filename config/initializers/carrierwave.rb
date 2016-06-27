416da89e6b393ec15a943656ed192722e15444a2
jbittner <jbittner@andrew.cmu.edu>
2016-06-27 03:02:47 -0400

    Carrierwave credentials included

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,9 @@ Fog.credentials_path = Rails.root.join('config/fog_credentials.yml')
 
 CarrierWave.configure do |config|
   config.fog_credentials = {
-      :provider => 'AWS'
+      :provider => 'AWS',
+      :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET_KEY']
   }
   config.fog_directory = "projectchuck2015" # name_of_bucket required
   config.asset_host    = 'http://s3.amazonaws.com'               # optional, defaults to nil
